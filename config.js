d7216faf72449fe6e308aace44646025807b078d
BillyElliot0811 <100996837+BillyElliot0811@users.noreply.github.com>
2022-11-20 20:47:09 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Thầy Cô Giáo';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20-11';
+const titleCard = 'Chúc Mừng Thầy Cô Giáo 20/11';
+const contentCard = 'Nhân ngày Nhà giáo Việt Nam 20/11, toàn thể lớp TĐH16A1CL xin gửi đến cô những lời cảm ơn chân thành nhất vì đã dìu dắt chúng em đến gần hơn với chân trời kiến thức. Kính chúc cô luôn luôn mạnh khỏe và hạnh phúc để luôn mang đến cho chúng em ngày càng nhiều bài học hay và bổ ích.';
+const giftImage = '112.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
